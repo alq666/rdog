a28ad50d750db00ef6a2f0d4aff3605db5f39441
Alexis Le-Quoc <alq@datadoghq.com>
2015-01-09 16:47:53 -0500

    Using slightly different json parsing packages

--- worksheet.r
+++ worksheet.r
@@ -1,5 +1,5 @@
 require(RCurl)
-require(rjson)
+require(jsonlite)
 require(zoo)
 
 to_epoch <- function(t) {
@@ -9,10 +9,10 @@ to_epoch <- function(t) {
 query_to_df <- function(api_key, application_key, query, from_t, to_t) {
   res <- getForm('https://app.datadoghq.com/api/v1/query', api_key=api_key, application_key=application_key, from=from_t, to=to_t, query=query)
   parsed <- fromJSON(res)
-  series <- parsed[['series']]
-  timeseries <- series[[1]]
+  timeseries <- parsed[['series']]
   pointlist <- timeseries[['pointlist']]
-  timestamps <- to_epoch(sapply(pointlist, '[', 1))
-  values <- sapply(pointlist, '[', 2)
-  return(zoo(values, order.by=timestamps))
+  df <- data.frame(pointlist[[1]])
+  ts <- mapply(to_epoch, df$X1)
+  df$X1 <- ts
+  return(df)
 }
