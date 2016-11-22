ade9317d9a252bcdf86db4802f1ede493e0341f4
Tony Tiger <thatrascaltiger@gmail.com>
2016-11-22 14:06:57 +0000

    Adding dog_metrics() function

---
+++ R/metrics.R
@@ -0,0 +1,20 @@
+#' Metrics 
+#'
+#' Return a list of all active metrics from given time until now
+#' @param api_key Your Datadog API key
+#' @param application_key Your Application Key
+#' @param from_t Start of time range 
+#' @export
+#' @examples
+#' dog_metrics('api_key', 'app_key', 1478872763)
+dog_metrics <- function(api_key, application_key, from_t) {
+    res <- RCurl::getForm("https://app.datadoghq.com/api/v1/metrics", 
+        api_key = api_key,
+        application_key = application_key,
+        from = from_t 
+    )
+    parsed <- jsonlite::fromJSON(res)
+
+    # TODO: format output
+    return(parsed$metrics)
+}
