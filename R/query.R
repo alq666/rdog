4b9746932ef364e792e286d22bd0e3b7422f37fc
Tony Tiger <thatrascaltiger@gmail.com>
2016-11-27 19:10:58 +0000

    Simplifying query logic by returning early

--- R/query.R
+++ R/query.R
@@ -19,37 +19,36 @@ dog_query <- function(api_key, application_key, query, from_t, to_t, as_df = FAL
         query = query
     )
     parsed <- jsonlite::fromJSON(res)
-    if (parsed[["status"]] == "ok") {
-        timeseries <- parsed[["series"]]  # data frame
-        scope <- timeseries[["scope"]]  # names of each group, if any
-        
-        # If * is the only scope, rename it to something else
-        if (length(scope) == 1 && scope == "*") {
-            scope <- "all"
-        }
-        pointlist <- timeseries$pointlist
-        # collect time parameters
-        start <- max(timeseries$start)
-        end <- max(timeseries$end)
-        interval <- min(timeseries$interval)
-        
-        
-        # Extract timestamps from the first list They will be identical across all groups
-        timestamps <- to_epoch(pointlist[[1]][, 1])
-        
-        # Collect all series values (accessible as [, 2])
-        v <- mapply(values, pointlist)
-        
-        # build the time series
-        if (as_df) {
-            df <- data.frame(timestamps, v)
-            colnames(df) <- c('time', scope)
-        } else {
-            df <- xts::xts(v, order.by = timestamps, frequency = freq(interval))
-            colnames(df) <- scope
-        }
+    if (parsed[["status"]] != "ok") {
+        # FIXME: Probably should handle failure better
+        return(xts())
+    }
+    timeseries <- parsed[["series"]]  # data frame
+    scope <- timeseries[["scope"]]  # names of each group, if any
+
+    # If * is the only scope, rename it to something else
+    if (length(scope) == 1 && scope == "*") {
+        scope <- "all"
+    }
+    pointlist <- timeseries$pointlist
+    # collect time parameters
+    start <- max(timeseries$start)
+    end <- max(timeseries$end)
+    interval <- min(timeseries$interval)
+
+    # Extract timestamps from the first list They will be identical across all groups
+    timestamps <- to_epoch(pointlist[[1]][, 1])
+
+    # Collect all series values (accessible as [, 2])
+    v <- mapply(values, pointlist)
+
+    # build the time series
+    if (as_df) {
+        df <- data.frame(timestamps, v)
+        colnames(df) <- c('time', scope)
     } else {
-        df <- xts()
+        df <- xts::xts(v, order.by = timestamps, frequency = freq(interval))
+        colnames(df) <- scope
     }
     return(df)
 }
